1035c3b6e47534dc36e5ff20f101d0c9d1a00df8
castrillejo <113512106+castrillejo@users.noreply.github.com>
2024-11-15 12:38:52 +0100

    dfe

---
+++ ecoembes/src/main/java/es/deusto/sd/ecoembes/EcoembesApplication.java
@@ -0,0 +1,17 @@
+package es.deusto.sd.ecoembes;
+
+//Comentario para probar el commit
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//prueba
+
+@SpringBootApplication
+public class EcoembesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoembesApplication.class, args);
+    }
+}
